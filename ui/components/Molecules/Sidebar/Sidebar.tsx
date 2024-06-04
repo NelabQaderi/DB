7037cc08a9a39078b77ab600693d5b7521973ed5
StarkBak <sstechnologyindtec@gmail.com>
2024-06-04 14:18:25 +0430

    all changes saved

--- ui/components/Molecules/Sidebar/Sidebar.tsx
+++ ui/components/Molecules/Sidebar/Sidebar.tsx
@@ -1,12 +1,22 @@
-'use client';
+"use client";
 
-import ThemeButton from '@/components/Atoms/ThemeButton/ThemeButton';
-import { AppShell, Avatar, Box, Burger, Button, Grid, Group, Skeleton, Text, Title } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import Link from 'next/link';
+import ThemeButton from "@/components/Atoms/ThemeButton/ThemeButton";
+import {
+  AppShell,
+  Avatar,
+  Box,
+  Burger,
+  Button,
+  Grid,
+  Group,
+  Skeleton,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
 
-
-export function Sidebar({ children } : { children: any }) {
+export function Sidebar({ children }: { children: any }) {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
 
@@ -15,17 +25,28 @@ export function Sidebar({ children } : { children: any }) {
       header={{ height: 60 }}
       navbar={{
         width: 300,
-        breakpoint: 'sm',
+        breakpoint: "sm",
         collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
       }}
       padding="md"
     >
       <AppShell.Header>
         <Group h="100%" px="md">
-          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-          <Avatar src="/logo.png" alt="it's me" /> 
-          <Title size={15}>Top in Town Technology Database</Title>
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
+          {/* <Avatar src="/logo.png" alt="it's me" /> 
+          <Title size={15}>Top in Town Technology Database</Title> */}
+          <Title size={15}>Database</Title>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
@@ -37,11 +58,33 @@ export function Sidebar({ children } : { children: any }) {
             <ThemeButton />
           </Grid.Col>
         </Grid>
-        <Button component={Link} href='/instructors' mb={5} fullWidth color='green'>Instructors</Button>
-        <Button component={Link} href='/classes' mb={5} fullWidth color='green'>Classes</Button>
-        <Button component={Link} href='/students' mb={5} fullWidth color='green'>Students</Button>
-        <Button component={Link} href='/report' mb={5} fullWidth color='green'>Report</Button>
-        <Button component={Link} href='/logout' mb={5} fullWidth color='green'>Log Out</Button>
+        <Button
+          component={Link}
+          href="/instructors"
+          mb={5}
+          fullWidth
+          color="green"
+        >
+          Instructors
+        </Button>
+        <Button component={Link} href="/classes" mb={5} fullWidth color="green">
+          Classes
+        </Button>
+        <Button
+          component={Link}
+          href="/students"
+          mb={5}
+          fullWidth
+          color="green"
+        >
+          Students
+        </Button>
+        <Button component={Link} href="/report" mb={5} fullWidth color="green">
+          Report
+        </Button>
+        <Button component={Link} href="/logout" mb={5} fullWidth color="green">
+          Log Out
+        </Button>
       </AppShell.Navbar>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
